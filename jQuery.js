93cfb0e65768c4f13f535e2db21de1c50d07462f
Phonon <8019825+phonon_QAQ@user.noreply.gitee.com>
2020-09-05 11:56:42 +0800

    添加了get,append,appendTo API

--- jQuery.js
+++ jQuery.js
@@ -6,6 +6,27 @@ window.$ = window.jQuery = function (selectorOrArray) {
     elements = selectorOrArray;
   }
   return {
+    get(index) {
+      return this.elements[index];
+    },
+    appendTo(node) {
+      if (node instanceof Element) {
+        this.each((el) => node.appendChild(el));
+      } else if (node.jquery === true) {
+        this.each((el) => node.get(0).appendChild(el));
+      }
+    },
+    append(children) {
+      if (children instanceof Element) {
+        this.get(0).appendChild(children);
+      } else if (children instanceof HTMLCollection) {
+        for (let i = 0; i < children.length; i++) {
+          this.get(0).appendChild(children[i]);
+        }
+      } else if (children.jquery === true) {
+        children.each((node) => this.get(0).appendChild(node));
+      }
+    },
     find(selector) {
       let array = [];
       for (let i = 0; i < elements.length; i++) {
